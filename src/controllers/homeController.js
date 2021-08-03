a40dab2e11bfecad424fbef5176baa20578c7483
Sinh Nguyen <sinhnguyen@MacBook-Pro-cua-Sinh.local>
2021-08-03 15:47:40 +0700

    Create table backend

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,23 @@
+import  db from "../models/index";
 
-let getHomePage=(req,res)=>{
-    return res.render("homepage.ejs")
-}
+let getHomePage = async (req,res) => {
+    try{
+    let data =await db.User.findAll();
+    console.log('---------------');
+    console.log(data);
+    console.log('---------------');
+    return res.render("homepage.ejs",{
+        data: JSON.stringify(data)
+    });
+} catch (e) {
+    console.log(e)
+}}
+
 let getAboutPage=(req,res)=>{
-    return res.render("test/about.ejs")
+    return res.render("test/about.ejs");
 }
 
-module.exports={
+module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage
 }
